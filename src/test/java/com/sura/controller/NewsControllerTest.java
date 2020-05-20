3dccf217debed020469e1b6b6e0ed8f8fb745529
Leeseonbeen <seonbeen121@gmail.com>
2020-05-20 13:08:19 +0900

    docker build

--- src/test/java/com/sura/controller/NewsControllerTest.java
+++ src/test/java/com/sura/controller/NewsControllerTest.java
@@ -6,8 +6,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 
