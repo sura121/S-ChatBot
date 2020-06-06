c572e6814a3322426e44c0c90a7bfa0c6fb6f369
Leeseonbeen <seonbeen121@gmail.com>
2020-06-06 17:32:35 +0900

    mapping post change

--- src/main/java/com/sura/controller/SChatBotController.java
+++ src/main/java/com/sura/controller/SChatBotController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class SChatBotController {
     @Autowired
     private MessageService messageService;
 
-    @GetMapping(value = "/hello")
+    @PostMapping(value = "/hello")
     public MessageVO HelloBot() {
         MessageVO message = new MessageVO();
 
