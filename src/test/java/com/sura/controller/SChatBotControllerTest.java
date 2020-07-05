55bd501825cf0db3f9fdb6befaa1b032f32f183a
Leeseonbeen <seonbeen121@gmail.com>
2020-07-05 22:37:27 +0900

    assertThat hamcrest apply

--- src/test/java/com/sura/controller/SChatBotControllerTest.java
+++ src/test/java/com/sura/controller/SChatBotControllerTest.java
@@ -12,15 +12,20 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
@@ -143,7 +148,16 @@ public class SChatBotControllerTest {
                         "}")
         ).andDo(print());
 
-
-
     }
+
+    @Test
+    public void testCode() {
+
+        int var = 10;
+        String nullTest = null;
+
+        System.out.println("asd");
+        assertThat(nullTest,is(nullValue()));
+    }
+
 }
